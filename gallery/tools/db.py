1b4119b84d0e7d34ee21c8d23c216969b6c8d990
Dair Azuaje <dza0042@auburn.edu>
2020-05-31 03:52:51 +0000

    Renamed db_program.py to user_admin.py. Refactored code for db.py and user_admin.py. Updated user_admin.py to edit users when selecting option 3

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -1,41 +1,115 @@
 import psycopg2
+from psycopg2 import OperationalError, errorcodes, errors
+
+# Configuration
 db_host = "demo-database-1.cyoqs8hmumyv.us-east-1.rds.amazonaws.com"
 db_name = "image_gallery"
 db_user = "image_gallery"
-
-connection = None
-
 password_file = "/home/ec2-user/.image_gallery_config"
+
+# SQL Queries
+sql_select_all = "SELECT * FROM users;"
+sql_add = "INSERT INTO users VALUES (%s, %s, %s);"
+sql_update_pw = "UPDATE users SET password = %s WHERE username = %s;"
+sql_update_fn = "UPDATE users SET full_name = %s WHERE username = %s;"
+sql_update_pw_fn = "UPDATE users SET full_name = %s, password = %s  WHERE username = %s;"
+sql_delete = "DELETE FROM users WHERE username = %s;"
+
 
 def get_password():
     f = open(password_file, "r")
     result = f.readline()
     f.close()
-    print("Password retrieved!")
     return result[:-1]
 
 def connect():
     global connection
-    connection = psycopg2.connect(host = db_host, dbname = db_name, user = db_user, password = get_password())
-    
-def execute(query, args = None):
+    try:
+        connection = psycopg2.connect(host = db_host, dbname = db_name, user = db_user, password = get_password())
+    except psycopg2.DatabaseError as error:
+        print(error)
+
+def select_all():
     global connection
-    cursor = connection.cursor()
-    if not args:
-        cursor.execute(query)
-    else:
-        cursor.execute(query, args)
-    return cursor
+    global cursor
+    try:
+        connection =  psycopg2.connect(host = db_host, dbname = db_name, user = db_user, password = get_password())
+        cursor = connection.cursor()
+        cursor.execute(sql_select_all)
+    
+        for row in cursor:
+            print(row)
 
-#def main():
-#    connect()
-#    res = execute("SELECT * FROM users;")
-#    for row in res:
-#        print(row)
+    except psycopg2.DatabaseError as error:
+        print(error)
 
-#    res = execute("UPDATE users SET password = %s WHERE username = 'dza0042';", ("Lincoln120217",))
-#    res = execute("SELECT * FROM users;")
-#    for row in res:
-#        print(row)
-#if __name__ == "__main__":
-#    main()
+    finally:
+        if connection is not None:
+            close()
+
+def delete(user):
+    global connection
+    global cursor
+    try:
+        connection =  psycopg2.connect(host = db_host, dbname = db_name, user = db_user, password = get_password())
+        cursor = connection.cursor()
+        cursor.execute(sql_delete, (user,))
+
+        if cursor.rowcount == 1:
+            print("User has been deleted")
+        elif cursor.rowcount == 0:
+            print("No such user.")
+
+    except psycopg2.DatabaseError as error:
+        print(error)
+
+    finally:
+        if connection is not None:
+            close()
+
+def add(user, pw, fn):       
+    global connection
+    global cursor
+    try:
+        connection =  psycopg2.connect(host = db_host, dbname = db_name, user = db_user, password = get_password())
+        cursor = connection.cursor()
+
+        cursor.execute(sql_add, (user, pw, fn,))
+        if cursor.rowcount != -1:
+            print("User has been added")
+        else: 
+            print("Error: User not added")
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            close()
+
+def edit(user, pw, fn):              
+    global connection
+    global cursor
+    try:
+        connection =  psycopg2.connect(host = db_host, dbname = db_name, user = db_user, password = get_password())
+        cursor = connection.cursor()
+        if pw == "" and fn != "":
+            cursor.execute(sql_update_fn, (fn, user,))
+        elif fn == "" and pw != "":
+            cursor.execute(sql_update_pw, (pw, user,))
+        elif fn != "" or pw != "":
+            cursor.execute(sql_update_pw_fn, (fn, pw, user,))
+
+        if cursor.rowcount != -1:
+            print("User has been edited")
+        else:
+            print("Error: User not updated")
+
+    except psycopg2.DatabaseError as error:
+        print(error)
+    finally:
+        if connection is not None:
+            close()
+            
+def close():
+    connection.commit()
+    cursor.close()
--- gallery/tools/user_admin.py
+++ gallery/tools/user_admin.py
@@ -4,7 +4,6 @@ from db import *
 def db_program():
     # Begin program and only run while option 5 is not entered
     program_run = True
-    connect()
     while (program_run):
         print("1) List users\n2) Add user\n3) Edit user\n4) Delete user\n5) Quit")
         prompt = "Enter command> "
@@ -14,30 +13,30 @@ def db_program():
         # Execute based on option
         if response == "1":
             # List all users using execute() from db.py
-            res = execute("SELECT * FROM users;")
-            for row in res:
-                print(row)
+            select_all()
             print()    
         elif response == "2":
             # Add user to database
             user = input("Username> ")
             pw = input("Password> ")
             fn = input("Full name> ")
-            execute("INSERT INTO users VALUES (%s, %s, %s);", (user, pw, fn,));
+            add(user, pw, fn);
             print()
         elif response == "3":
             user = input("Username to edit> ")
+            pw = input("New password (press enter to keep current)> ")
+            fn = input ("New full name (press enter to keep current)> ")
+
+            edit(user, pw, fn)
             # If user does not exist in DB, say "No such user."
         elif response == "4":
             user = input("Enter username to delete> ")
-
+            
             # Check if user is real
             yes_no = input("Are you sure you want to delete " + user + "? ")
             if yes_no == "yes":
                 # If yes, delete. If no, do nothing
-                res = execute("DELETE FROM users WHERE username = %s;", (user,))
-                if res.rowcount == 0:
-                    print("\nNo such user.")
+                delete(user)
                 print()
         elif response == "5":
             program_run = False

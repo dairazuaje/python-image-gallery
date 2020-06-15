efa712e0ae5bae2e79b5347e81fdd3f374f016a7
EC2 Default User <ec2-user@ip-10-0-1-88.ec2.internal>
2020-06-15 01:54:42 +0000

    Updated program to have a gui using Python, Flask, and HTML.

---
+++ gallery/ui/app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+from db import *
+from secrets import *
+
+app = Flask(__name__)
+
+
+@app.route("/admin")
+def admin():
+    users = select_all()
+    close()
+    return render_template("admin.html", users=users)
+
+
+@app.route("/admin/edit_user_form<fn>")
+def edit_user(fn):
+    user = fn
+    return render_template("edit_user_form.html", user=user)
+
+
+@app.route("/admin/process_edit_user_form<user>", methods=["POST"])
+def process_edit_user_form(user):
+    # username = request.form['username']
+    password = request.form['password']
+    fullname = request.form["fullname"]
+    edit(user, password, fullname)
+    return render_template("process_edit_user_form.html", user=user)
+
+
+@app.route("/admin/add_user_form")
+def add_user_form():
+    return render_template("add_user_form.html")
+
+
+@app.route("/admin/process_user_add_form", methods=["POST"])
+def process_add_user_form():
+    username = request.form['username']
+    password = request.form['password']
+    fullname = request.form["fullname"]
+    add(username, password, fullname)
+    return render_template("process_add_user_form.html", user=username)
+
+
+@app.route("/admin/process_delete_user_form<user>", methods=["POST"])
+def process_delete_user_form(user):
+    delete(user)
+    return render_template("process_delete_user_form.html", user=user)
--- gallery/ui/db.py
+++ gallery/ui/db.py
@@ -4,10 +4,10 @@ from psycopg2 import OperationalError, errorcodes, errors
 from secrets import get_secret_image_gallery
 
 # Configuration
-#db_host = "image-gallery.cyoqs8hmumyv.us-east-1.rds.amazonaws.com"
-#db_name = "image_gallery"
-#db_user = "image_gallery"
-#password_file = "/home/ec2-user/.image_gallery_config"
+# db_host = "image-gallery.cyoqs8hmumyv.us-east-1.rds.amazonaws.com"
+# db_name = "image_gallery"
+# db_user = "image_gallery"
+# password_file = "/home/ec2-user/.image_gallery_config"
 
 # SQL Queries
 sql_select_all = "SELECT * FROM users;"
@@ -18,31 +18,37 @@ sql_update_pw_fn = "UPDATE users SET full_name = %s, password = %s  WHERE userna
 sql_delete = "DELETE FROM users WHERE username = %s;"
 sql_columns = "SELECT * FROM users LIMIT 0"
 
+
 def get_secret():
-    print("trying to connect to secrets manager")
     jsonString = get_secret_image_gallery()
-    print("retrieved from secrets manager")
     return json.loads(jsonString)
+
 
 def get_password(secret):
     return secret["password"]
 
+
 def get_host(secret):
     return secret["host"]
+
 
 def get_username(secret):
     return secret["username"]
 
+
 def get_database(secret):
     return secret["database_name"]
+
 
 def connect():
     global connection
     secret = get_secret()
     try:
-        connection = psycopg2.connect(host = get_host(secret), dbname = get_database(secret), user = get_username(secret), password = get_password(secret))
+        connection = psycopg2.connect(host=get_host(secret), dbname=get_database(secret), user=get_username(secret),
+                                      password=get_password(secret))
     except psycopg2.DatabaseError as error:
         print(error)
+
 
 def select_all():
     connect()
@@ -50,21 +56,9 @@ def select_all():
     global cursor
 
     cursor = connection.cursor()
-
     cursor.execute(sql_select_all)
-    colnames = [desc[0] for desc in
-                cursor.description]  # Retrieves column names from database to then display with SELECT * results
-
-    for column in colnames:
-        print("{: <25}".format(column.upper()), end = " ")
-
-    print()
     result = cursor.fetchall()
-    for row in result:
-        #print(row)
-        print("{: <25} {: <25} {: <25}".format(*row))
-
-    close()
+    return result
 
 def delete(user):
     connect()
@@ -72,7 +66,6 @@ def delete(user):
     global cursor
 
     cursor = connection.cursor()
-
     if check_user(user):
         cursor.execute(sql_delete, (user,))
         if cursor.rowcount == 1:
@@ -82,6 +75,7 @@ def delete(user):
 
     close()
 
+
 def add(user, pw, fn):
     connect()
     global connection
@@ -90,12 +84,14 @@ def add(user, pw, fn):
     cursor = connection.cursor()
 
     cursor.execute(sql_add, (user, pw, fn,))
+    print("user added")
     if cursor.rowcount != -1:
         print("User has been added")
     else:
         print("Error: User not added")
 
     close()
+
 
 def edit(user, pw, fn):
     connect()
@@ -121,18 +117,22 @@ def edit(user, pw, fn):
 
     close()
 
-            
-def check_user(user):
 
+def check_user(user):
+    print("I AM GETTING CALLED!")
+    print(user)
     cursor = connection.cursor()
     sql = "SELECT * FROM users WHERE username = %s;"
     cursor.execute(sql, (user,))
     if cursor.rowcount > 0:
+        print("YEP")
         return True
     else:
+        print("NOPE")
         return False
         print(error)
-        
+
+
 def close():
     connection.commit()
     cursor.close()

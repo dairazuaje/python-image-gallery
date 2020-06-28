85a6962eb8fef7e9f4cf80e1bb17f8670e8667ed
EC2 Default User <ec2-user@ip-10-0-1-125.ec2.internal>
2020-06-28 04:12:04 +0000

    Modified db.py to work without secrets for the time being while ansible finishes getting set up

--- gallery/ui/db.py
+++ gallery/ui/db.py
@@ -19,32 +19,33 @@ sql_delete = "DELETE FROM users WHERE username = %s;"
 sql_columns = "SELECT * FROM users LIMIT 0"
 
 
-def get_secret():
-    jsonString = get_secret_image_gallery()
-    return json.loads(jsonString)
+#def get_secret():
+#    jsonString = get_secret_image_gallery()
+#    return json.loads(jsonString)
 
 
-def get_password(secret):
-    return secret["password"]
+#def get_password(secret):
+#    return secret["password"]
 
 
-def get_host(secret):
-    return secret["host"]
+#def get_host(secret):
+#    return secret["host"]
 
 
-def get_username(secret):
-    return secret["username"]
+#def get_username(secret):
+#    return secret["username"]
 
 
-def get_database(secret):
-    return secret["database_name"]
+#def get_database(secret):
+#    return secret["database_name"]
 
 
 def connect():
     global connection
-    secret = get_secret()
+    #secret = get_secret()
     try:
-        connection = psycopg2.connect(host=get_host(secret), dbname=get_database(secret), user=get_username(secret),
+        connection = psycopg2.connect(host = db_host, dbname = db_name, user = db_user, password = get_password())
+        #connection = psycopg2.connect(host=get_host(secret), dbname=get_database(secret), user=get_username(secret),
                                       password=get_password(secret))
     except psycopg2.DatabaseError as error:
         print(error)
